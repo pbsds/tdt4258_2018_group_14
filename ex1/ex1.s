031ba9fda03610cd776a076b747972ad7f3cf5bd
pbsds <pbsds@hotmail.com>
2018-09-12 17:15:36 +0200

    Add comments

--- ex1/ex1.s
+++ ex1/ex1.s
@@ -116,51 +116,61 @@ _reset:
 
 
 
+// The main loop
 main:
     // read button states
     ldr r0, =GPIO_PC_BASE
     ldr r8, [r0, GPIO_DIN]              //- read button states
 
+    // compare with last button state to determine if the button was newly pressed
     mvn r1, r9
     mov r9, r8
     orr r8, r1, r8
 
     //do shifting
-    and r1, r8, #0x1
-    cbnz r1, main_2
+    and r1, r8, #0x1                    //- check if button SW1 is pressed
+    cbnz r1, main_2                     //- skip shiftleft if not pressed
     bl shiftleft
 main_2:
-    and r1, r8, #0x4
-    cbnz r1, main_3
+    and r1, r8, #0x4                    //- check if button SW2 is pressed
+    cbnz r1, main_3                     //- skip shiftright if not pressed
     bl shiftright
 main_3:
-    and r1, r8, #0x2
-    cbnz r1, main_4
+    and r1, r8, #0x2                    //- check if button SW3 is pressed
+    cbnz r1, main_4                     //- skip light if not pressed
     bl light
 main_4:
-    and r1, r8, #0x8
-    cbnz r1, main_5
+    and r1, r8, #0x8                    //- check if button SW4 is pressed
+    cbnz r1, main_5                     //- skip lightnegate if not pressed
     bl lightnegate
 main_5:
 
-    ldr r0, =GPIO_PA_BASE
-    lsl r1, r7, 8
+    ldr r0, =GPIO_PA_BASE               //- Load in entry point for LEDs
+    lsl r1, r7, 8                       //- Right shift the current light position
 
-    cmp r10, #1
-    beq main_6
-    mvn r1, r1
+    cmp r10, #1                         //- Check if you should negate the light
+    beq main_6                          //- Skip if you are not supposed to negate
+    mvn r1, r1                          //- Negate the bits, inverting the light
 main_6:
-    str r1, [r0, GPIO_DOUT]
+    str r1, [r0, GPIO_DOUT]             //- Set the LEDs to the right state
+
+
+    b main                              //- loop unconditionally
 
 
 
-    b main                              //- loop
+////////////////////////////////////////////////////////////////////////////
+//
+//  Functions
+//
+////////////////////////////////////////////////////////////////////////////
+
 
 shiftleft:
     push {LR}
-    cmp r7, #0x01
-    beq shiftleft_end
-    lsr r7, r7, 1
+    cmp r7, #0x01                       //- Check if light already all to the left
+    beq shiftleft_end                   //- Skip if light can't be moved any more to the left
+    lsr r7, r7, 1                       //- Move light to the left
 shiftleft_end:
     pop {LR}                            //- return
     mov PC, LR
@@ -168,9 +178,9 @@ shiftleft_end:
     .thumb_func
 shiftright:
     push {LR}
-    cmp r7, #0x80
-    beq shiftright_end
-    lsl r7, r7, 1
+    cmp r7, #0x80                       //- Check if light already all to the right
+    beq shiftright_end                  //- Skip if light can't be moved any more to the right
+    lsl r7, r7, 1                       //- Move light to the right
 shiftright_end:
     pop {LR}                            //- return
     mov PC, LR
@@ -178,7 +188,7 @@ shiftright_end:
     .thumb_func
 light:
     push {LR}
-    mov r10, #1
+    mov r10, #1                         //- Set light to be positive
     pop {LR}                            //- return
     mov PC, LR
 
@@ -186,13 +196,13 @@ light:
     .thumb_func
 lightnegate:
     push {LR}
-    mov r10, #0
+    mov r10, #0                         //- Set light to be negated
     pop {LR}                            //- return
     mov PC, LR
 
 
 data:
-    .word 0                             //- led position
+    .word 0                             //- derp
 
   /////////////////////////////////////////////////////////////////////////////
   //
@@ -204,10 +214,10 @@ data:
         .thumb_func
 gpio_handler:
 
-        b .  // do nothing
+        b .                             //- do nothing
 
   /////////////////////////////////////////////////////////////////////////////
 
         .thumb_func
 dummy_handler:
-        b .  // do nothing
+        b .                             //- do nothing

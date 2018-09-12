e334fcfbab3628d5f54b8b75e89557823babd779
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2018-09-12 11:51:07 +0200

    Fix whitespacing

--- ex1/efm32gg.s
+++ ex1/efm32gg.s
@@ -1,24 +1,24 @@
-	/////////////////////////////////////////////////////////////////////////////
-	//
+  /////////////////////////////////////////////////////////////////////////////
+  //
   // Various useful I/O addresses and definitions for EFM32GG
-	//
-	// TODO: Add more registers
-	//
-	/////////////////////////////////////////////////////////////////////////////
+  //
+  // TODO: Add more registers
+  //
+  /////////////////////////////////////////////////////////////////////////////
 
 
-	      //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
       	// GPIO
 
-	      GPIO_PA_BASE = 0x40006000
-	      GPIO_PB_BASE = 0x40006024
-	      GPIO_PC_BASE = 0x40006048
-	      GPIO_PD_BASE = 0x4000606c
-	      GPIO_PE_BASE = 0x40006090
-	      GPIO_PF_BASE = 0x400060b4
+        GPIO_PA_BASE = 0x40006000
+        GPIO_PB_BASE = 0x40006024
+        GPIO_PC_BASE = 0x40006048
+        GPIO_PD_BASE = 0x4000606c
+        GPIO_PE_BASE = 0x40006090
+        GPIO_PF_BASE = 0x400060b4
 
-	      // register offsets from base address
-	      GPIO_CTRL     = 0x00
+        // register offsets from base address
+        GPIO_CTRL     = 0x00
         GPIO_MODEL    = 0x04
         GPIO_MODEH    = 0x08
         GPIO_DOUT     = 0x0c
@@ -28,20 +28,20 @@
         GPIO_DIN      = 0x1c
         GPIO_PINLOCKN = 0x20
 
-	      GPIO_BASE = 0x40006100
+        GPIO_BASE = 0x40006100
 
-	      // register offsets from base address
+        // register offsets from base address
         GPIO_EXTIPSELL = 0x00
         GPIO_EXTIPSELH = 0x04
         GPIO_EXTIRISE  = 0x08
         GPIO_EXTIFALL  = 0x0c
-	      GPIO_IEN       = 0x10
-	      GPIO_IF        = 0x14
-	      GPIO_IFC       = 0x1c
+        GPIO_IEN       = 0x10
+        GPIO_IF        = 0x14
+        GPIO_IFC       = 0x1c
 
-	      //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
       	// CMU
-	
+  
         CMU_BASE = 0x400c8000
 
         CMU_HFPERCLKDIV = 0x008
@@ -49,8 +49,8 @@
 
         CMU_HFPERCLKEN0_GPIO = 13
 
-	      //////////////////////////////////////////////////////////////////////
-	      // NVIC
+        //////////////////////////////////////////////////////////////////////
+        // NVIC
 
         ISER0 = 0xe000e100
         ISER1 = 0xe000e104
@@ -68,15 +68,14 @@
         IPR2 = 0x08
         IPR3 = 0x0c
 
-	      //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
         // EMU
 
-	      EMU_BASE = 0x400c6000
+        EMU_BASE = 0x400c6000
 
         EMU_CTRL = 0x000
 
-	      //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
         // System Control Block
 
         SCR = 0xe000ed10
-
